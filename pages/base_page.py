41aefc9f76e128c93aa0d3c4ba3f5e34038f3070
anton <baksfire@yandex.ru>
2025-05-12 00:19:18 +0500

    Создание conftest.py, создание тестjd на открытие страниц через горизонтальное меню главного экрана

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page, Response
+from playwright.sync_api import Page, Response, expect
 from data import base_data
 from typing import Optional
 
@@ -10,4 +10,19 @@ class BasePage:
         self.base_url: str = base_data.base_url
 
     def open(self, uri: str) -> Optional[Response]:
+        """
+        Переход на конкретную страницу сайта
+        """
         return self.page.goto(f"{self.base_url}{uri}")
+
+    def click_locator(self, loc):
+        """
+        Клик на локатор
+        """
+        return self.page.locator(loc).click()
+
+    def expect_locator(self, loc):
+        """
+        Ожидание локатора
+        """
+        return expect(self.page.locator(loc)).to_be_visible()
--- tests/test_main_page/test_horizontal_menu.py
+++ tests/test_main_page/test_horizontal_menu.py
@@ -1,8 +1,49 @@
 from playwright.sync_api import Page
 from pages.main_page.horizontal_menu_page import HorizontalMenuPage
+import pytest
 
 
+@pytest.mark.smoke
 class TestHorizontalMenu:
+
+    slides = HorizontalMenuPage
+
     def test_debit_cards(self, page: Page):
-        debit = HorizontalMenuPage(page)
+        debit = self.slides(page)
         debit.open_debit_cards()
+
+    def test_credit_cards(self, page: Page):
+        credit = self.slides(page)
+        credit.open_credit_cards()
+
+    def test_credits(self, page: Page):
+        credit = self.slides(page)
+        credit.open_credits()
+
+    def test_premium(self, page: Page):
+        premium = self.slides(page)
+        premium.open_premium()
+
+    def test_open_deposits(self, page: Page):
+        deposits = self.slides(page)
+        deposits.open_deposits()
+
+    def test_open_investment(self, page: Page):
+        investment = self.slides(page)
+        investment.open_investment()
+
+    def test_open_sim_card(self, page: Page):
+        sim_card = self.slides(page)
+        sim_card.open_sim_card()
+
+    def test_open_insurance(self, page: Page):
+        insurance = self.slides(page)
+        insurance.open_insurance()
+
+    def test_open_journeys(self, page: Page):
+        journeys = self.slides(page)
+        journeys.open_journeys()
+
+    def test_open_for_business(self, page: Page):
+        for_business = self.slides(page)
+        for_business.open_everything_for_business()
